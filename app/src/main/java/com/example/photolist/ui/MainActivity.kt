12659e608e2d2f08afaf6166d9e23ae73f3455c7
Aditya <aditya@plexoc.com>
2023-01-06 22:16:07 +0530

    - Setup project structure - Added necessary dependencies - Enabled data-binding

--- app/src/main/java/com/example/photolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/photolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.photolist
+package com.example.photolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
